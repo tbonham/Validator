3ebfca9515388684732f6be7df5aaf061ce4b251
Thomas Bonham <thomasbonham@outlook.com>
2018-09-27 06:01:51 -0700

    Adding nuget packages for project

--- Validator/Validator/Validator.cs
+++ Validator/Validator/Validator.cs
@@ -6,5 +6,17 @@ namespace Validator
 {
     public static class Validator
     {
+        #region FirstName
+        public static Boolean FirstName(String strValue)
+        {
+            return false;
+        }
+
+        public static Boolean FirstNameCustom(String strPatten,String strValue)
+        {
+            return false;
+        }
+
+        #endregion
     }
 }
