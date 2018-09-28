d32f0c8c6b303d4cefe743b5401815bf8931e072
Thomas Bonham <thomasbonham@outlook.com>
2018-09-28 05:12:08 -0700

    Adding SanitizeGeneralInput function. It will remove most bad things from a string

--- Validator/Validator/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Validator
-{
-    public class Class1
-    {
-    }
-}
--- Validator/Validator/Validator.cs
+++ Validator/Validator/Validator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Validator
 {
@@ -12,11 +13,36 @@ namespace Validator
             return false;
         }
 
-        public static Boolean FirstNameCustom(String strPatten,String strValue)
+        public static Boolean FirstNameCustom(String strPatten, String strValue)
         {
             return false;
         }
 
         #endregion
+
+        #region LastName
+
+        #endregion
+
+        /// <summary>
+        /// SanitizeGeneralInput is a general function that will remove most degest items from a string.
+        /// It can also remove good things.
+        /// So Make sure you test everything well.
+        /// </summary>
+        public static String SanitizeGeneralInput(String value)
+        {
+            String strRegex = @"[^\w\.@-]";
+            try
+            {
+                return Regex.Replace(value, strRegex,
+                    "",
+                    RegexOptions.None,
+                    TimeSpan.FromSeconds(3.0));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return String.Empty;
+            }
+        }
     }
 }
